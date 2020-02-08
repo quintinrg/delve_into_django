6c475401526c5f71f3fbbbc1fe4fe83e79a215dc
aikilink <quintinrg@protonmail.com>
2020-02-08 15:29:54 -0600

    Continued tutorial, and initialized database with SQLite. Migrated Question and Choice classes, and confirmed working relationships in interactive shell.

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+# This function actually displays the text below. This is considered a "view"
+x = "42"
+
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    return HttpResponse("Hello, world. You're at the polls index. " + x)
